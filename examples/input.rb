d25173da3c1100a6c8b834ee31fe4be2f51747fd
Ari Russo <ari.russo@gmail.com>
2011-03-19 19:15:42 -0400

    examples

--- examples/input.rb
+++ examples/input.rb
@@ -6,10 +6,10 @@ require 'alsa-rawmidi'
 # this program selects the first midi input and sends an inspection of the first 10 messages 
 # messages it receives to standard out
 
+num_messages = 10
+
 # AlsaRawMIDI::Device.all.to_s will list your midi outputs
 # or amidi -l from the command line
-
-num_messages = 10
 
 AlsaRawMIDI::Device.first(:input).enable do |input|
 
