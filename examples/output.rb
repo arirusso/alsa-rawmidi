244559ec1ab7379179f22810fcd4831f7bcb9ea0
Ari Russo <ari.russo@gmail.com>
2011-03-19 19:14:01 -0400

    output example

--- examples/output.rb
+++ examples/output.rb
@@ -9,19 +9,21 @@ require 'alsa-rawmidi'
 # or amidi -l from the command line
 
 output = AlsaRawMIDI::Device.first(:output)
-output.enable
+output.enable do |output|
 
-notes = [36, 40, 43] # C E G
-octaves = 6
-duration = 0.1
+	notes = [36, 40, 43] # C E G
+	octaves = 6
+	duration = 0.1
 
-5.times do |i|
-	notes.each do |note|
-		oct = i * 12
-		note_message = [0x90, note + oct, 100]
-		output.output_message(note_message)
-		sleep(duration)
-		off_message = [0x80, note + oct, 100]
-		output.output_message(off_message)
+	5.times do |i|
+		notes.each do |note|
+			oct = i * 12
+			note_message = [0x90, note + oct, 100]
+			output.output_message(note_message)
+			sleep(duration)
+			off_message = [0x80, note + oct, 100]
+			output.output_message(off_message)
+		end
 	end
+	
 end
