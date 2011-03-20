da34083db61e19f6f2138b3a64eff913c527d106
Ari Russo <ari.russo@gmail.com>
2011-03-19 20:27:51 -0400

    test cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require 'alsa-rawmidi'
 module TestHelper
 	    
     def bytestrs_to_ints(arr)
-		data = arr.map { |m| m[:data] }.join
+    	data = arr.map { |m| m[:data] }.join
     	output = []
     	until (bytestr = data.slice!(0,2)).eql?("")
     	  output << bytestr.hex
@@ -15,19 +15,21 @@ module TestHelper
        	output    	
     end
     
+    # some MIDI messages
     VariousMIDIMessages = [
-      [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], # SysEx message
-      [0x90, 100, 100],
-      [0x90, 43, 100],
-      [0x90, 76, 100],
-      [0x90, 60, 100],
-      [0x90, 45, 100]
+      [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], # SysEx
+      [0x90, 100, 100], # note on
+      [0x90, 43, 100], # note on
+      [0x90, 76, 100], # note on
+      [0x90, 60, 100], # note on
+      [0x80, 100, 100] # note off
     ]
     
+    # some MIDI messages
     VariousMIDIByteStrMessages = [
-      "F04110421240007F0041F7", # SysEx message
-      "906440",
-      "904340"
+      "F04110421240007F0041F7", # SysEx
+      "906440", # note on
+      "804340" # note off
     ]
     
 end
