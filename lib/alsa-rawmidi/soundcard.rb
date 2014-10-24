2cbe6628d44f4498e94da3a97ac5af1c8c27a856
Ari Russo <ari.russo@gmail.com>
2014-10-23 21:00:06 -0400

    extract methods

--- lib/alsa-rawmidi/soundcard.rb
+++ lib/alsa-rawmidi/soundcard.rb
@@ -6,21 +6,7 @@ module AlsaRawMIDI
 
     def initialize(card_num)
       @subdevices = { :input => [], :output => [] }
-      name = "hw:#{card_num}"
-      handle_ptr = FFI::MemoryPointer.new(FFI.type_size(:int))
-      #snd_ctl = Map::SndCtl.new
-      #snd_ctl_pointer = FFI::MemoryPointer.new(:pointer).write_pointer(snd_ctl.pointer)
-      API.snd_ctl_open(handle_ptr, name, 0)
-      handle = handle_ptr.read_int
-      32.times do |i|
-        devnum = FFI::MemoryPointer.new(:int).write_int(i)
-        if (err = API.snd_ctl_rawmidi_next_device(handle, devnum)) < 0
-        break # fix this
-        end
-        @subdevices.keys.each do |type|
-          populate_subdevices(type, handle, card_num, i)
-        end
-      end
+      populate_subdevices(card_num)
     end
 
     def self.find(card_num)
@@ -28,6 +14,22 @@ module AlsaRawMIDI
     end
 
     private
+
+    def populate_subdevices(card_num)
+      name = "hw:#{card_num}"
+      handle_ptr = FFI::MemoryPointer.new(FFI.type_size(:int))
+      API.snd_ctl_open(handle_ptr, name, 0)
+      handle = handle_ptr.read_int
+      ids = (0..31).to_a.select do |i|
+        devnum = FFI::MemoryPointer.new(:int).write_int(i)
+        API.snd_ctl_rawmidi_next_device(handle, devnum) >= 0
+      end
+      ids.each do |id|
+        @subdevices.keys.each do |direction|
+          populate_subdevice(direction, handle, card_num, id)
+        end
+      end
+    end
 
     def unpack(string)
       arr = string.delete_if { |n| n.zero? }
@@ -39,36 +41,45 @@ module AlsaRawMIDI
       "hw:#{card_num},#{device_num}#{ext}"
     end
 
-    def populate_subdevices(type, ctl_ptr, card_num, device_num)
-      ctype, dtype = *case type
-      when :input then [API::CONSTANTS[:SND_RAWMIDI_STREAM_INPUT], Input]
-      when :output then [API::CONSTANTS[:SND_RAWMIDI_STREAM_OUTPUT], Output]
+    def populate_subdevice(direction, ctl_ptr, card_num, device_num)
+      stream_key = case direction
+      when :input then :SND_RAWMIDI_STREAM_INPUT
+      when :output then :SND_RAWMIDI_STREAM_OUTPUT
       end
+      stream = API::CONSTANTS[stream_key]
       info = API::SndRawMIDIInfo.new
       API.snd_rawmidi_info_set_device(info.pointer, device_num)
-      API.snd_rawmidi_info_set_stream(info.pointer, ctype)
+      API.snd_rawmidi_info_set_stream(info.pointer, stream)
       i = 0
       subdev_count = 1
       available = []
-      while (i <= subdev_count)
+      while i <= subdev_count
         API.snd_rawmidi_info_set_subdevice(info.pointer, i)
-        # fix this
-        if (err = API.snd_ctl_rawmidi_info(ctl_ptr, info.pointer)) < 0
-        break
-        end
+        if API.snd_ctl_rawmidi_info(ctl_ptr, info.pointer) >= 0
 
-        if (i < 1)
-        subdev_count = API.snd_rawmidi_info_get_subdevices_count(info.pointer)
-        subdev_count = (subdev_count > 32) ? 0 : subdev_count
-        end
+          if i.zero?
+            subdev_count = API.snd_rawmidi_info_get_subdevices_count(info.pointer)
+            subdev_count = 0 if subdev_count > 32
+          end
 
-        name = info[:name].to_s
-        system_id = get_alsa_subdev_id(card_num, device_num, subdev_count, i)
-        dev = dtype.new(:system_id => system_id, :name => info[:name].to_s, :subname => info[:subname].to_s)
-        available << dev
-        i += 1
+          available << new_device(direction, info, card_num, device_num, subdev_count, i)
+          i += 1
+        else
+          break
+        end
       end
-      @subdevices[type] += available
+      @subdevices[direction] += available
+    end
+
+    # Instantiate a new device object
+    # @return [Input, Output]
+    def new_device(direction, info, card_num, device_num, subdev_count, id)
+      system_id = get_alsa_subdev_id(card_num, device_num, subdev_count, id)
+      device_class = case direction
+      when :input then Input
+      when :output then Output
+      end
+      device_class.new(:system_id => system_id, :name => info[:name].to_s, :subname => info[:subname].to_s)
     end
 
   end
